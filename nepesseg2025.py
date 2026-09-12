7fd0de3d40a3e931dd5777f1b941c99f58655511
KSzabi01 <183347398+KSzabi01@users.noreply.github.com>
2026-09-12 17:20:26 +0200

    1 megcsinálva

--- nepesseg2025.py
+++ nepesseg2025.py
@@ -1,4 +1,5 @@
 database = []
+varos_tipusok = ["város", "vármegyei jogú város", "vármegye székhely", "fővárosi kerület"]
 
 with open('lakossag_2025.csv', 'r', encoding='utf-8') as forrasfajl:
     forrasfajl.readline()
@@ -13,10 +14,59 @@ with open('lakossag_2025.csv', 'r', encoding='utf-8') as forrasfajl:
         }
         database.append(telepules_adatai)
 
+def megye_adatai(kapott_megyekod):
+    megye_telepulesei = []
+    for i in database:
+        if i["megyekod"] == kapott_megyekod:
+            megye_telepulesei.append(i)
+    
+    if megye_telepulesei == []:
+        return
+    telepulesek_szama_megye = len(megye_telepulesei)
+    ossz_lelekszam = 0
+    varos_lelekszam = 0
+    for i in megye_telepulesei:
+        if i["telepules"] == varos_tipusok[0:]:
+            varos_lelekszam += i["ferfi"] + i["no"]
+        ossz_lelekszam += i["ferfi"] + i["no"]
+    osszes_adat = {
+        "telepulesek_szama": telepulesek_szama_megye,
+        "lelekszam": ossz_lelekszam,
+        "v_lelekszam": varos_lelekszam
+    }
+    return osszes_adat
+
 
 def main():
+    print()
+    while True:
+        print("Népesség 2025")
+        print("--------\n Főmenü\n--------\n [1] Megye adatai \n [2] Település típusai \n [x] Kilépés")
+        valasztas = input("Írja be a menüpont előtti karaktert, majd nyomjon [Enter]-t: ")
+        match valasztas:
+            case "x" | "X":
+                break
+            case "1":
+                while True:
+                    bekert_megye_kod = input("Adjon meg egy megyekódot (A megye első három karaktere, ékezet nélkül): ").upper()
+                    megye_adatok = megye_adatai(bekert_megye_kod)
+                    print()
+                    if megye_adatok:
+                        print(f"Települések száma a megyében: {megye_adatok["telepulesek_szama"]} db \n Megyében élők száma: {megye_adatok["lelekszam"]} fő \n Városokban élők száma: {megye_adatok["v_lelekszam"]} fő")
+                        print()
+                        break
+                    else:
+                        print("Nincs ilyen megyekód!\n\n [x] Térjen vissza a főmenübe\n [Space] Próbálja újra")
+                        megye_ujra = input("Írja be a menüpont előtti karaktert, majd nyomjon [Enter]-t: ")
+                        print()
+                        if megye_ujra.lower() == "x":
+                            break
+            case "2":
+                pass
+            case _:
+                print("\nNincs ilyen opció!\n")
 
-    return
+main()
 
 # "Adjon meg egy megyekódot (A megye első három karaktere, ékezet nélkül): "
 # f"Települések száma a megyében: {változó} db \n Megyében élők száma: {változó} fő \n Városokban élők száma: {változó} fő"
